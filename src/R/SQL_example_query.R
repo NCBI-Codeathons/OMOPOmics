8fcc8b56ccac3812bf50bf9675f6567da2b1095a
Alex Francette <amf198@pitt.edu>
2020-01-10 13:52:30 -0500

    revising sample queries

--- src/R/SQL_example_query.R
+++ src/R/SQL_example_query.R
@@ -1,18 +1,11 @@
 # setwd to OMOPomics
 setwd("")
-library(DBI)
-library (tidyverse)
-library(dplyr)
-library(dbplyr)
-library(RSQLite)
-library(lubridate)
-library(RMySQL)
+ 
 
 # connects to SQL database
 con <- DBI::dbConnect(RSQLite::SQLite(), "OMOP_tables.sqlite")
 # lists tables in database
 dbListTables(con)
-
 
 # loads database tables into R
 assay_occurrence_data <- tbl(con,"assay_occurrence_data")
@@ -25,47 +18,30 @@ specimen <- tbl(con,"specimen_table")
 # if you wish to add parameters for sequencing details, do so in the assay occurence table
 # assay_occurrence_parameters <- tbl(con,"assay_occurrence_parameters")
 
-# assay_occurrence_data <- read.csv(file = "OMOP_tables_copy/assay_occurrence_data.txt", header = T, sep = "\t")
-# assay_occurrence_parameters <- read.csv(file = "OMOP_tables_copy/assay_occurrence_parameters.txt", header = T, sep = "\t")
-# assay_occurrence <- read.csv(file = "OMOP_tables_copy/assay_occurrence.txt", header = T, sep = "\t")
-# condition_occurrence <- read.csv(file = "OMOP_tables_copy/condition_occurrence.txt", header = T, sep = "\t")
-# person <- read.csv(file = "OMOP_tables_copy/person.txt", header = T, sep = "\t")
-# provider <- read.csv(file = "OMOP_tables_copy/provider.txt", header = T, sep = "\t")
-# specimen <- read.csv(file = "OMOP_tables_copy/specimen.txt", header = T, sep = "\t")
 
-# # EXAMPLE QUERY 1
+# # EXAMPLE QUERY 1 ALL FEMALE PATIENTS W/ ATAC-seq
 # merge sample and person data
-merge_0_a <- person %>% 
-  inner_join(specimen, by = "person_id") %>% 
-  distinct()
-
-merge_0_b <- filter(merge_0_a, gender_source_value == "female")
-
-merge_0_c <- 
-  
-# finding male patients...
 merge_1_a <- person %>% 
-  filter(gender_source_value == "male") %>% 
   inner_join(specimen, by = "person_id") %>% 
   distinct()
 
-# with T-cell activation
-merge_1_b <- condition_occurrence %>%
-  filter(condition_type_value == "T cell activation") %>% 
-  select(person_id,condition_type_value) %>% 
-  distinct() %>% 
-  inner_join(merge_1_a, by="person_id") %>% 
+# female patients only 
+merge_1_b <- filter(merge_0_a, gender_source_value == "female") %>% 
   distinct()
 
 # with associated ATAC-seq data
-merge_1_c <- inner_join(merge_1_b, assay_occurrence %>% filter(assay_source_value == "ATAC"), by="specimen_source_value")
+merge_1_c <- inner_join(merge_1_b, assay_occurrence %>% filter(assay_source_value == "ATAC"), by="specimen_source_value") %>% 
+  distinct()
 
+?inner_join()
 # collect ATAC-seq peak file paths 
-merge_1_d <- inner_join(merge_1_c, assay_occurrence_data, by="assay_occurrence_id")
-male_TCA_ATAC <- merge_1_d$file_source_value
+merge_1_d <- inner_join(merge_1_c, assay_occurrence_data, by="specimen_id")
+merge_1_d_comp <- compute(merge_1_d)
+merge_1_d_coll <- collect(merge_1_d_comp)
+all_CTCL_ATAC <- merge_1_e_coll$file_source_value
 
 # export list of filepaths of ATAC-seq data
-write.csv(male_TCA_ATAC, file = "data/cohorts/male_TCA_ATAC.csv")
+write.csv(all_CTCL_ATAC, file = "data/cohorts/all_CTCL_ATAC.csv")
 
 #_______________________________________________________________
 
@@ -85,12 +61,14 @@ merge_2_b <-specimen %>%
 merge_2_c <- assay_occurrence %>% 
   filter(assay_source_value == "ATAC") %>% 
   distinct() %>% 
-  inner_join(merge_2_b, by="specimen_source_value") %>% 
+  inner_join(merge_2_b, by="specimen_id") %>% 
   distinct()
 
 # collect ATAC-seq peak file paths 
-merge_2_d <- inner_join(merge_2_c, assay_occurrence_data, by="assay_occurrence_id")
-all_CTCL_ATAC <- merge_2_d$file_source_value
+merge_2_d <- inner_join(merge_2_c, assay_occurrence_data, by="specimen_id")
+merge_2_d_comp <- compute(merge_2_d)
+merge_2_d_coll <- collect(merge_2_d_comp)
+all_CTCL_ATAC <- merge_2_d_coll$file_source_value
 
 # export list of filepaths of ATAC-seq data
 write.csv(all_CTCL_ATAC, file = "data/cohorts/all_CTCL_ATAC.csv")
@@ -145,6 +123,42 @@ dbFetch(res)
 
 dbDisconnect(con)
 
+
+# assay_occurrence_data <- read.csv(file = "OMOP_tables_copy/assay_occurrence_data.txt", header = T, sep = "\t")
+# assay_occurrence_parameters <- read.csv(file = "OMOP_tables_copy/assay_occurrence_parameters.txt", header = T, sep = "\t")
+# assay_occurrence <- read.csv(file = "OMOP_tables_copy/assay_occurrence.txt", header = T, sep = "\t")
+# condition_occurrence <- read.csv(file = "OMOP_tables_copy/condition_occurrence.txt", header = T, sep = "\t")
+# person <- read.csv(file = "OMOP_tables_copy/person.txt", header = T, sep = "\t")
+# provider <- read.csv(file = "OMOP_tables_copy/provider.txt", header = T, sep = "\t")
+# specimen <- read.csv(file = "OMOP_tables_copy/specimen.txt", header = T, sep = "\t")
+
+
+# with CTCL
+merge_0_c <- condition_occurrence %>%
+  filter(condition_type_value == "cutaneous T cell leukemia (CTCL)") %>% 
+  select(person_id, condition_type_value) %>%
+  distinct() %>%
+  inner_join(merge_0_b, by="person_id") %>% 
+  distinct()
+
+# finding male patients...
+merge_1_a <- person %>% 
+  filter(gender_source_value == "male") %>% 
+  inner_join(specimen, by = "person_id") %>% 
+  distinct()
+
+# with T-cell activation
+merge_1_b <- condition_occurrence %>%
+  filter(condition_type_value == "T cell activation") %>% 
+  select(person_id,condition_type_value) %>% 
+  distinct() %>% 
+  inner_join(merge_1_a, by="person_id") %>% 
+  distinct()
+
+
+
+# export list of filepaths of ATAC-seq data
+write.csv(male_TCA_ATAC, file = "data/cohorts/male_TCA_ATAC.csv")
 
 
 
