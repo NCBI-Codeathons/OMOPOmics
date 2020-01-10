fa3f608bb374559c2a6b85ce0abe601d167f2a4a
Alex Francette <amf198@pitt.edu>
2020-01-10 12:20:08 -0500

    added flowchart pptx and continued to revise query code

--- src/R/SQL_example_query.R
+++ src/R/SQL_example_query.R
@@ -4,13 +4,13 @@ library (tidyverse)
 library(dplyr)
 library(dbplyr)
 library(RSQLite)
+library(lubridate)
 #install.packages(c("dbplyr", "RSQLite"))
 download.file(url = "https://ndownloader.figshare.com/files/2292171",
               destfile = "data/example_SQL_db/portal_mammals.sqlite", mode = "wb")
 
-# con <- DBI::dbConnect(RSQLite::SQLite(), "data/example_SQL_db/portal_mammals.sqlite")
-# dbListTables(con)
-
+con <- DBI::dbConnect(RSQLite::SQLite(), "data/GSE60682_db/sql_database.sql")
+dbListTables(con)
 # assay_occurrence_data <- tbl(con,"assay_occurrence_data")
 # assay_occurrence_parameters <- tbl(con,"assay_occurrence_parameters")
 # assay_occurrence <- tbl(con,"assay_occurrence")
@@ -18,6 +18,8 @@ download.file(url = "https://ndownloader.figshare.com/files/2292171",
 # person <- tbl(con,"person")
 # provider <- tbl(con,"provider")
 # specimen <- tbl(con,"specimen")
+
+?SQLite
 
 assay_occurrence_data <- read.csv(file = "OMOP_tables_copy/assay_occurrence_data.txt", header = T, sep = "\t")
 assay_occurrence_parameters <- read.csv(file = "OMOP_tables_copy/assay_occurrence_parameters.txt", header = T, sep = "\t")
@@ -82,37 +84,46 @@ write.csv(all_CTCL_ATAC, file = "data/cohorts/all_CTCL_ATAC.csv")
 
 #_______________________________________________________________
 
-# # EXAMPLE QUERY 3 SEARCH FOR PATIENTS WITH 
-# finding CTCL patients...
-merge_2_a <- condition_occurrence %>%
-  filter(condition_source_value == "cutaneous T cell leukemia (CTCL)") %>% 
-  inner_join(person, by = "person_id") %>% 
+# # EXAMPLE QUERY 3 SEARCH FOR FEMALE DATA REGARDLESS OF CONDITION
+# finding female patients
+merge_3_a <- person %>% 
+  filter(gender_source_value == "female") %>% 
+  inner_join(specimen, by = "person_id") %>% 
   distinct()
 
-# adding associated specimen_source_value to match against assay_occurance tables
-merge_2_b <-specimen %>% 
-  inner_join(merge_2_a, by="person_id") %>% 
+### NEEDS WORK 
+
+#_______________________________________________________________
+
+# # EXAMPLE QUERY 4 ALL SAMPLES WITH A RAPID TIMECOURSE
+# finding all patients
+merge_4_a <- person %>%
+  inner_join(specimen, by = "person_id") %>% 
   distinct()
 
-# selecting patients with ATAC-seq data
-merge_2_c <- assay_occurrence %>% 
-  filter(assay_source_value == "ATAC") %>% 
+# with T-cell activation
+merge_4_b <- condition_occurrence %>%
+  filter(condition_source_value == "T cell activation") %>% 
+  select(person_id,condition_source_value) %>% 
   distinct() %>% 
-  inner_join(merge_2_b, by="specimen_source_value") %>% 
+  inner_join(merge_4_a, by="person_id") %>% 
   distinct()
 
-# collect ATAC-seq peak file paths 
-merge_2_d <- inner_join(merge_2_c, assay_occurrence_data, by="assay_occurrence_id")
-all_CTCL_ATAC <- merge_2_d$file_source_value
-
-# export list of filepaths of ATAC-seq data
-write.csv(all_CTCL_ATAC, file = "data/cohorts/all_CTCL_ATAC.csv")
-
-
+# selecting patients with ATAC-seq data and timecourse data within 4 hour window
+merge_4_c <- assay_occurrence %>% 
+  filter(as.numeric(hms(assay_start_date)) <= 14400) %>% 
+  distinct() %>% 
+  inner_join(merge_4_b, by="specimen_source_value") %>% 
+  distinct()
 
 
+pat 	<- "45 14:43:16"
+vals	<- str_match(pat,pattern="(^[:digit:]+) ([:digit:]{2}):([:digit:]{2}):([:digit:]{2})$")
+day 	<- as.integer(vals[2])
 
 
+?str_match
+a
 # # EXAMPLE QUERY 3
 # isolates specimen_source_values of everyone that did not fall into query 1 
 
